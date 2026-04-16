22acb358acd93927f48b2825e6e68e17527f98aa
fabioscarda <fabio.scarda@gmail.com>
2026-04-16 17:21:39 +0200

    git action

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import {defineConfig, loadEnv} from 'vite';
 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
-  base: '/QuizApp/';
   return {
+    base: './QuizApp/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
